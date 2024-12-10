43e0f561f547e89169521262ec72cf3b0bf059e0
류혜선 <142039244+Ryuhyeseon@users.noreply.github.com>
2024-12-10 01:40:52 +0000

    standard input output

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> // 헤더 파일(표준 입출력 정의 라이브러리 포함)
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main(void) { // 메인 함수(return type : int, argument : void)
+  printf("Hello World\n"); // 문자열 출력
+  return 0; // return value
 }
