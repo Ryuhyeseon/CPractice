833bcbc17f873d42899e0de5e0796f6f1fca5d1f
류혜선 <142039244+Ryuhyeseon@users.noreply.github.com>
2024-12-17 03:36:46 +0000

    Structure

--- .ccls-cache/@home@runner@CPractice/main.c
+++ .ccls-cache/@home@runner@CPractice/main.c
@@ -1,17 +1,36 @@
 #include <stdio.h> // 헤더 파일(표준 입출력 정의 라이브러리 포함)
 
+// Structure
+struct Person {
+  int age;
+  char *name;
+};
+
 // C 언어의 컴파일러는 main 함수가 딱 하나만 있어야 한다고 기대하며, main 함수가
 // 여러 번 정의되어 있으면 링커 단계에서 오류가 발생한다.
 int main(void) { // 메인 함수(return type : int, argument : void)
 
+  // Structure
+  struct Person personStructure;
+  struct Person* personStructurePointer;
+
+  personStructurePointer = &personStructure;
+  
+  personStructure.age = 25;
+  printf("personStructure.age : %d\n", personStructure.age);
+  printf("personStruturePointer->age : %d\n", personStructurePointer->age);
+
+  personStructure.name = "류혜선";
+  printf("personStructure.name : %s\n", personStructure.name);
+  printf("personStructurePointer->name : %s\n", personStructurePointer->name);
+  
   // standard input output
-  //   printf("Hello World\n"); // 문자열 출력
-  //   return 0; // return value
+  // printf("Hello World\n"); // 문자열 출력
+  return 0; // return value
 
   // variable
-  //   int x = 10; //variable data type 명시
-  //   printf("x : %d", x);
-  //   return 0;
+  // int x = 10; //variable data type 명시
+  // printf("x : %d", x);
 
   // operator
   // printf("10 / 3 = %d\n", 10 / 3);
@@ -70,8 +89,9 @@ int main(void) { // 메인 함수(return type : int, argument : void)
   // printf("num : %d\n", num);
 
   // User Defined Function
-  printf("User Defined Function returnNum_10 : %d", returnNum_10());
+  // printf("User Defined Function returnNum_10 : %d", returnNum_10());
+  
 }
 
 // User Defined Function
-int returnNum_10() { return 10; }
+// int returnNum_10() { return 10; }
