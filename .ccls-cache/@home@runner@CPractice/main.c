b76101b79d11a06c5e6f05e9f6eed68d1a9f16d6
류혜선 <142039244+Ryuhyeseon@users.noreply.github.com>
2024-12-17 05:24:14 +0000

    Array

--- .ccls-cache/@home@runner@CPractice/main.c
+++ .ccls-cache/@home@runner@CPractice/main.c
@@ -1,36 +1,36 @@
 #include <stdio.h> // 헤더 파일(표준 입출력 정의 라이브러리 포함)
 
 // Structure
-struct Person {
-  int age;
-  char *name;
-};
+// struct Person {
+//   int age;
+//   char *name;
+// };
 
 // C 언어의 컴파일러는 main 함수가 딱 하나만 있어야 한다고 기대하며, main 함수가
 // 여러 번 정의되어 있으면 링커 단계에서 오류가 발생한다.
 int main(void) { // 메인 함수(return type : int, argument : void)
 
   // Structure
-  struct Person personStructure;
-  struct Person* personStructurePointer;
+  // struct Person personStructure;
+  // struct Person* personStructurePointer;
 
-  personStructurePointer = &personStructure;
+  // personStructurePointer = &personStructure;
   
-  personStructure.age = 25;
-  printf("personStructure.age : %d\n", personStructure.age);
-  printf("personStruturePointer->age : %d\n", personStructurePointer->age);
+  // personStructure.age = 25;
+  // printf("personStructure.age : %d\n", personStructure.age);
+  // printf("personStruturePointer->age : %d\n", personStructurePointer->age);
 
-  personStructure.name = "류혜선";
-  printf("personStructure.name : %s\n", personStructure.name);
-  printf("personStructurePointer->name : %s\n", personStructurePointer->name);
+  // personStructure.name = "류혜선";
+  // printf("personStructure.name : %s\n", personStructure.name);
+  // printf("personStructurePointer->name : %s\n", personStructurePointer->name);
 
-  personStructurePointer->name  = "update 류혜선";
-  printf("personStructurePointer->name : %s\n", personStructurePointer->name);
-  printf("personStructure.name : %s\n", personStructure.name);
+  // personStructurePointer->name  = "update 류혜선";
+  // printf("personStructurePointer->name : %s\n", personStructurePointer->name);
+  // printf("personStructure.name : %s\n", personStructure.name);
   
   // standard input output
   // printf("Hello World\n"); // 문자열 출력
-  return 0; // return value
+  // return 0; // return value
 
   // variable
   // int x = 10; //variable data type 명시
@@ -94,6 +94,16 @@ int main(void) { // 메인 함수(return type : int, argument : void)
 
   // User Defined Function
   // printf("User Defined Function returnNum_10 : %d", returnNum_10());
+
+  // Array
+  int a[2][2] = {{11, 22},{33, 44}};
+  int *p;
+  p = a[0];
+  
+  printf("*(p) : %d\n", *(p));
+  printf("*(p+1) : %d\n", *(p+1));
+  printf("*(p+2) : %d\n", *(p+2));
+  printf("*(p+3) : %d\n", *(p+3));
   
 }
 
